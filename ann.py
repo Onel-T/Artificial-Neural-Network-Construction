bf16dd18923b993925f95c842e3b1c4f5391d6b3
Onel-T <90119008+Onel-T@users.noreply.github.com>
2023-03-18 02:11:42 -0700

    added creation of network

--- ann.py
+++ ann.py
@@ -1,9 +1,9 @@
 class node:
     def __init__(self, connections):
-        self.connection = connections
+        self.connections = connections
         self.collector = 0.0
 
-# Returns list of numbers from from csv
+# Returns list of numbers from csv
 def strip_csv(csv):
     list_of_nums = []
 
@@ -18,9 +18,44 @@ def strip_csv(csv):
             list_of_nums.append(float(n))
 
     return list_of_nums
+
+
+def create_layer(number_of_nodes, last_layer):
+    new_layer = []
+    # Loop amount of nodes
+    for i in range(number_of_nodes):
+        new_layer.append(node(last_layer))
+    
+    return new_layer
+
+
+def create_connections(network):
+    # Connecting layer n to layer n+1 (last layer is connected to none)
+    for layer in range(len(network) - 1):
+        for node_in_layer in range(len(network[layer])):
+            network[layer][node_in_layer].connections = network[layer + 1]
+
+
+def create_network(nodes_per_layer):
+    last_layer = None
+    network = []
+
+    # Loop amount of elements (layers)
+    for num_nodes in nodes_per_layer:
+        network.append(create_layer(num_nodes, last_layer))
+
+    create_connections(network)
+
+    return network
+
     
 
 if __name__ == '__main__':
     nodes_per_layer = strip_csv("network.txt")
     input_nums = strip_csv("input.txt")
+
+    network = create_network(nodes_per_layer)
+
+
+
 
